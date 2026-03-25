aa28aa6c3cca5e6d9213310cca89d6a73c505d7d
Ekallavya <madhureddie@gmail.com>
2026-03-25 10:50:07 +0530

    ELK changes applied

--- ProductService/src/main/java/com/dailycodebuffer/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/com/dailycodebuffer/ProductService/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.dailycodebuffer.ProductService.controller;
 import com.dailycodebuffer.ProductService.model.ProductRequest;
 import com.dailycodebuffer.ProductService.model.ProductResponse;
 import com.dailycodebuffer.ProductService.service.ProductService;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
+@Log4j2
 public class ProductController {
 
     @Autowired
@@ -19,6 +22,7 @@ public class ProductController {
     //@PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest) {
+        log.info("Adding product {}", productRequest);
         Long productId = productService.addProduct(productRequest);
         return new ResponseEntity<>(productId, HttpStatus.CREATED);
     }
